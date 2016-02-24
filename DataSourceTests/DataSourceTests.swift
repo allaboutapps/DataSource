b233ab2b9d363b103d11b148bc8399197f2c79b7
Marcel Zanoni <marcel@luniclabs.at>
2016-02-24 10:00:09 +0100

    DataSource, TableViewCellConfigurator and some TableViewDataSource tests

--- DataSourceTests/DataSourceTests.swift
+++ DataSourceTests/DataSourceTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+import UIKit
+
 @testable import DataSource
 
 class DataSourceTests: XCTestCase {
@@ -21,16 +23,149 @@ class DataSourceTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testNumberOfSections() {
+        let dataSource1 = singleSectionDataSource()
+        XCTAssertEqual(dataSource1.sections.count, 1, "number of sections = 1")
+        
+        let dataSource2 = multipleSectionDataSource()
+        XCTAssertEqual(dataSource2.sections.count, 2, "number of sections = 2")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testNumberOfRows() {
+        let dataSource1 = singleSectionDataSource()
+        XCTAssertEqual(dataSource1.sectionAtIndex(0).numberOfRows, 4, "number of rows = 4")
+        
+        let dataSource2 = multipleSectionDataSource()
+        XCTAssertEqual(dataSource2.sectionAtIndex(1).numberOfRows, 3, "number of rows = 3")
     }
     
+    func testFirstAndLastSection() {
+        let dataSource = twoEmpySectionDataSource()
+        
+        let section1 = dataSource.sectionAtIndex(0)
+        let section2 = dataSource.sectionAtIndexPath(NSIndexPath(forRow: 0, inSection: 1))
+        
+        XCTAssertEqual(dataSource.firstSection?.title, section1.title, "first section")
+        XCTAssertEqual(dataSource.lastSection?.title, section2.title, "last section")
+    }
+    
+    func testMutatingSections() {
+        var dataSource = twoEmpySectionDataSource()
+        let section1 = dataSource.sectionAtIndex(0)
+        let section2 = dataSource.sectionAtIndex(1)
+        
+        // remove
+        dataSource.removeSectionAtIndex(0)
+        XCTAssertEqual(dataSource.numberOfSections, 1, "number of sections = 1")
+        
+        // insert
+        dataSource.insertSection(section1, index: 1)
+        XCTAssertEqual(dataSource.numberOfSections, 2, "number of sections = 2")
+        
+        // set
+        dataSource.setSection(section1, index: 0)
+        dataSource.setSection(section2, index: 1)
+        XCTAssertEqual(dataSource.sectionAtIndex(0).title, section1.title, "")
+        XCTAssertEqual(dataSource.sectionAtIndex(1).title, section2.title, "")
+        
+        // append section
+        dataSource.appendSection(section1)
+        XCTAssertEqual(dataSource.numberOfSections, 3, "number of sections = 3")
+        XCTAssertEqual(dataSource.lastSection?.title, section1.title, "")
+        
+        // append dataSource
+        dataSource.appendDataSource(twoEmpySectionDataSource())
+        XCTAssertEqual(dataSource.numberOfSections, 5, "number of sections = 5")
+        XCTAssertEqual(dataSource.lastSection?.title, section2.title, "")
+    }
+    
+    func testRowCreatorAndCountClosures() {
+        let dataSource = DataSource(Section(rowCountClosure: { () -> Int in
+            return 1
+            }, rowCreatorClosure: { (rowIndex) -> Row<Any> in
+                return Row(identifier: RowIdentifier.Text.rawValue, data: "a")
+        }))
+        
+        XCTAssertEqual(dataSource.sectionAtIndex(0).numberOfRows, 1, "number of rows = 1")
+        
+        let row = dataSource.rowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))
+        XCTAssertEqual(row.anyData as? String, "a", "row.anyData = a")
+    }
+    
+    func testSectionHasTitle() {
+        let sectionWithTitle = Section<Any>(title: "Title", rows: [])
+        let sectionWithoutTitle = Section<Any>(title: nil, rows: [])
+        let sectionWithEmptyTitle = Section<Any>(title: "", rows: [])
+        
+        XCTAssertEqual(sectionWithTitle.hasTitle, true, "section.hasTitle = true")
+        XCTAssertEqual(sectionWithoutTitle.hasTitle, false, "section.hasTitle = false")
+        XCTAssertEqual(sectionWithEmptyTitle.hasTitle, false, "section.hasTitle = false")
+    }
+    
+    func testAlternativeInit() {
+        let section1 = Section<Any>(title: "Section", rows: [])
+        let dataSource1 = DataSource(section1)
+        
+        XCTAssertEqual(dataSource1.numberOfSections, 1, "number of sections = 1")
+        
+        let dataSources = [singleSectionDataSource(), singleSectionDataSource()]
+        let dataSource2 = DataSource(dataSources: dataSources)
+        
+        XCTAssertEqual(dataSource2.numberOfSections, 2, "number of sections = 2")
+    }
+    
+    func testRowDataStrings() {
+        let dataSource1 = singleSectionDataSource()
+        
+        XCTAssertEqual(dataSource1.sectionAtIndex(0).numberOfRows, 4, "number of rows")
+        
+        let row0: Row<String> = dataSource1.rowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))
+        XCTAssertEqual(row0.data, "a", "row_0_0: 1")
+        
+        let row1: Row<String> = dataSource1.rowAtIndexPath(NSIndexPath(forRow: 1, inSection: 0))
+        XCTAssertEqual(row1.data, "b", "row_0_1: 2")
+        
+        let row2: Row<String> = dataSource1.rowAtIndexPath(NSIndexPath(forRow: 2, inSection: 0))
+        XCTAssertEqual(row2.data, "c", "row_0_2: 3")
+        
+        let row3: Row<String> = dataSource1.rowAtIndexPath(NSIndexPath(forRow: 3, inSection: 0))
+        XCTAssertEqual(row3.data, "d", "row_0_3: 4")
+    }
+    
+}
+
+extension DataSourceTests {
+    
+    func singleSectionDataSource() -> DataSource {
+        return DataSource([
+            Section(rows: [
+                Row(identifier: "Text", data: "a"),
+                Row(identifier: "Text", data: "b"),
+                Row(identifier: "Text", data: "c"),
+                Row(identifier: "Text", data: "d"),
+                ])
+            ])
+    }
+    
+    func multipleSectionDataSource() -> DataSource {
+        return DataSource([
+            Section(rows: [
+                Row(identifier: "Text", data: "0a"),
+                Row(identifier: "Text", data: "0b"),
+                Row(identifier: "Text", data: "0c"),
+                ]),
+            Section(rows: [
+                Row(identifier: "Text", data: "1a"),
+                Row(identifier: "Text", data: "2b"),
+                Row(identifier: "Text", data: "3c"),
+                ])
+            ])
+    }
+    
+    func twoEmpySectionDataSource() -> DataSource {
+        let section1 = Section<Any>(title: "First", rows: [])
+        let section2 = Section<Any>(title: "Last", rows: [])
+        
+        return DataSource([section1, section2])
+    }
 }
---
+++ DataSourceTests/TableViewCellConfiguratorTests.swift
@@ -0,0 +1,141 @@
+//
+//  TableViewCellConfiguratorTests.swift
+//  Example
+//
+//  Created by Marcel Zanoni on 22/02/16.
+//  Copyright © 2016 aaa - all about apps GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import DataSource
+
+class TableViewCellConfiguratorTests: XCTestCase {
+    
+    let person1 = Person(firstname: "Matthias", lastname: "Buchetics")
+    let person2 = Person(firstname: "Marcel", lastname: "Zanoni")
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testTableViewDataSourceSectionTitle() {
+        let tableViewDataSource = personTableViewDataSource()
+        
+        let sectionTitle = tableViewDataSource.sectionAtIndex(0).title
+        XCTAssertEqual(sectionTitle, "Persons", "section title = Persons")
+    }
+    
+    func testSingleRowTypeSectionTableViewDataSource() {
+        let tableViewDataSource = personTableViewDataSource()
+        
+        let row = tableViewDataSource.rowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))
+        
+        let configurator = tableViewDataSource.configuratorForRowIdentifier(RowIdentifier.Person.rawValue)
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: RowIdentifier.Person.rawValue)
+        
+        configurator?.configureRow(row, cell: cell, indexPath: NSIndexPath(forRow: 0, inSection: 0))
+        XCTAssertEqual(cell.textLabel?.text, person1.firstname, "cell.textLabel.text = Matthias")
+        XCTAssertEqual(cell.detailTextLabel?.text, person1.lastname, "cell.detailTextLabel.text = Buchetics")
+    }
+    
+    func testMixedRowTypeSectionTableViewDataSource() {
+        let tableViewDataSource = personAndStringsTableViewDataSource()
+        
+        let personRow = tableViewDataSource.rowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))
+        let personConfigurator = tableViewDataSource.configuratorForRowIdentifier(RowIdentifier.Person.rawValue)
+        
+        let personCell = UITableViewCell(style: .Subtitle, reuseIdentifier: RowIdentifier.Person.rawValue)
+        
+        personConfigurator?.configureRow(personRow, cell: personCell, indexPath: NSIndexPath(forRow: 0, inSection: 0))
+        XCTAssertEqual(personCell.textLabel?.text, person1.firstname, "cell.textLabel.text = Matthias")
+        XCTAssertEqual(personCell.detailTextLabel?.text, person1.lastname, "cell.detailTextLabel.text = Buchetics")
+        
+        let stringRow = tableViewDataSource.rowAtIndexPath(NSIndexPath(forRow: 1, inSection: 0))
+        let stringConfigurator = tableViewDataSource.configuratorForRowIdentifier(RowIdentifier.Text.rawValue)
+        
+        let stringCell = UITableViewCell(style: .Default, reuseIdentifier: RowIdentifier.Text.rawValue)
+        
+        stringConfigurator?.configureRow(stringRow, cell: stringCell, indexPath: NSIndexPath(forRow: 1, inSection: 0))
+        XCTAssertEqual(stringCell.textLabel?.text, "a", "cell.textLabel.text = a")
+    }
+    
+    func testRowAndCellIdentifierTableViewDataSource() {
+        let dataSource = personAndStringDataSource()
+        let cellIdentifier = "Text"
+        
+        let tableViewDataSource = TableViewDataSource(dataSource: dataSource, configurators: [
+            TableViewCellConfigurator(rowIdentifier: RowIdentifier.Person.rawValue, cellIdentifier: cellIdentifier, configure: { (person: Person, cell: UITableViewCell, indexPath: NSIndexPath) in
+                cell.textLabel?.text = "\(person.firstname) \(person.lastname)"
+            }),
+            TableViewCellConfigurator(rowIdentifier: RowIdentifier.Text.rawValue, cellIdentifier: cellIdentifier, configure: { (text: String, cell: UITableViewCell, indexPath: NSIndexPath) in
+                cell.textLabel?.text = text
+            })
+            ])
+        
+        let personIndexPath = NSIndexPath(forRow: 0, inSection: 0)
+        let stringIndexPath = NSIndexPath(forRow: 1, inSection: 0)
+        
+        let tableView = UITableView()
+        tableView.dataSource = tableViewDataSource
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        let personCell = tableViewDataSource.tableView(tableView, cellForRowAtIndexPath: personIndexPath)
+        XCTAssertEqual(personCell.textLabel?.text, "\(person1.firstname) \(person1.lastname)", "cell.textLabel.text = Matthias Buchetics")
+        
+        let stringCell = tableViewDataSource.tableView(tableView, cellForRowAtIndexPath: stringIndexPath)
+        XCTAssertEqual(stringCell.textLabel?.text, "a", "cell.textLabel.text = a")
+    }
+    
+}
+
+extension TableViewCellConfiguratorTests {
+    
+    func personDataSource() -> DataSource {
+        return DataSource([
+            Section<Person> (title: "Persons", rowIdentifier: RowIdentifier.Person.rawValue, rows: [
+                person1,
+                person2
+                ])
+            ])
+    }
+    
+    func personAndStringDataSource() -> DataSource {
+        return DataSource([
+            Section<Any> (title: "Mixed", rows: [
+                Row(identifier: RowIdentifier.Person.rawValue, data: person1),
+                Row(identifier: RowIdentifier.Text.rawValue, data: "a")
+                ])
+            ])
+    }
+    
+    func personTableViewDataSource() -> TableViewDataSource {
+        let dataSource = personDataSource()
+        
+        return TableViewDataSource(dataSource: dataSource, configurator:
+            TableViewCellConfigurator<Person, UITableViewCell>(RowIdentifier.Person.rawValue) { (person: Person, cell: UITableViewCell, _) in
+                cell.textLabel?.text = person.firstname
+                cell.detailTextLabel?.text = person.lastname
+            })
+    }
+    
+    func personAndStringsTableViewDataSource() -> TableViewDataSource {
+        let dataSource = personAndStringDataSource()
+        
+        return TableViewDataSource(dataSource: dataSource, configurators: [
+            TableViewCellConfigurator<Person, UITableViewCell>(RowIdentifier.Person.rawValue) { (person: Person, cell: UITableViewCell, _) in
+                cell.textLabel?.text = person.firstname
+                cell.detailTextLabel?.text = person.lastname
+            },
+            TableViewCellConfigurator<String, UITableViewCell>(RowIdentifier.Text.rawValue) { (title: String, cell: UITableViewCell, _) in
+                cell.textLabel?.text = title
+            }
+            ])
+    }
+}
